6812d9953f1a8505b3808a8f5cf367caeed5a706
soreeq <soreeq@gmail.com>
2023-01-20 13:27:28 +0100

    first commit

---
+++ src/main/java/winprediction/WinpredictionApplication.java
@@ -0,0 +1,13 @@
+package winprediction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinpredictionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinpredictionApplication.class, args);
+	}
+
+}
