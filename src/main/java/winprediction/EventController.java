2bf01669c64e681a91fc5a629fa54850d3d3842b
soreeq <soreeq@gmail.com>
2023-01-20 14:47:59 +0100

    implement method that return 10most probable results

--- src/main/java/winprediction/EventController.java
+++ src/main/java/winprediction/EventController.java
@@ -14,19 +14,45 @@ import java.util.List;
 @Controller
 public class EventController {
     @GetMapping("/event")
-    public String showEvent(ModelMap modelMap) throws IOException {
+    public String showEvent(ModelMap modelMap) throws Exception {
 
         byte[] jsonData = Files.readAllBytes(Paths.get("C:\\Users\\gszaw\\OneDrive\\Pulpit\\prediction of winnings\\src\\main\\resources\\json\\data.json"));
         String jsonString = new String(jsonData);
 
         ObjectMapper mapper = new ObjectMapper();
         Events events = mapper.readValue(jsonString, Events.class);
-//        List<Event> eventList = new ArrayList<>();
-//        eventList.add(event);
+        List<Event> eventList = new ArrayList<>();
+
+        int counter = 0;
         for (Event event : events.getEvents()){
-            System.out.println(event.getSportEventId());
+            eventList.add(event);
+            String dateString = event.getStartDate();
+            event.setStartDate(dateString.replace("T", " ").replace(":00+00:00", " "));
+
+            String homeTeam = event.getCompetitors().get(0).getName();
+            String awayTeam = event.getCompetitors().get(1).getName();
+
+            double homeTeamProb = event.getProbabilityHomeTeamWinner();
+            double awayTeamProb = event.getProbabilityAwayTeamWinner();
+
+            if (homeTeamProb > awayTeamProb) {
+                event.setWillProbablyWin(homeTeam);
+                event.setHighestProbability(homeTeamProb);
+            } else if (homeTeamProb == awayTeamProb) {
+                event.setWillProbablyWin("draw");
+                event.setHighestProbability(homeTeamProb);
+            } else {
+                event.setWillProbablyWin(awayTeam);
+                event.setHighestProbability(awayTeamProb);
+            }
+
+            counter++;
+            if(counter == 10)
+                break;
         }
-//        modelMap.addAttribute(eventList);
-        return "info.html";
+
+        modelMap.addAttribute("events", eventList);
+
+        return "info";
     }
 }
