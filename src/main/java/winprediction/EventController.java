0587c129cb1c8c687a5880794a66bdc8756aa3b1
soreeq <soreeq@gmail.com>
2023-01-20 15:06:54 +0100

    prepare method to handle the number of expected matches given as a parameter

--- src/main/java/winprediction/EventController.java
+++ src/main/java/winprediction/EventController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -14,7 +15,7 @@ import java.util.List;
 @Controller
 public class EventController {
     @GetMapping("/event")
-    public String showEvent(ModelMap modelMap) throws Exception {
+    public String showEvent(ModelMap modelMap, @RequestParam(value = "limit", defaultValue = "10") int numberOfMatchesToDisplay) throws Exception {
 
         byte[] jsonData = Files.readAllBytes(Paths.get("C:\\Users\\gszaw\\OneDrive\\Pulpit\\prediction of winnings\\src\\main\\resources\\json\\data.json"));
         String jsonString = new String(jsonData);
@@ -46,11 +47,18 @@ public class EventController {
                 event.setHighestProbability(awayTeamProb);
             }
 
+            if(numberOfMatchesToDisplay > events.getEvents().size()){
+                numberOfMatchesToDisplay = events.getEvents().size();
+            }
+
+            System.out.println(numberOfMatchesToDisplay);
+
             counter++;
-            if(counter == 10)
+            if(counter == numberOfMatchesToDisplay)
                 break;
         }
 
+        modelMap.addAttribute("numberOfMatchesToDisplay", numberOfMatchesToDisplay);
         modelMap.addAttribute("events", eventList);
 
         return "info";
